934b857d93bc699d901835ece52724752a8ed378
Quentin Coloos <quentin.coloos@gmail.com>
2017-05-11 11:33:20 +0200

    Mise a jour du CMakeLists avec les TU et Logs

--- src/chifoumi.cpp
+++ src/chifoumi.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
- 
-int main()
+#include <glog/logging.h>
+
+int main(int argc, char ** argv)
 {
+  ////////////////Initialisation Logs////////////////////
+  google::InitGoogleLogging("log_chifoumi");
+  google::SetLogDestination(google::GLOG_INFO, "log_chifoumi");
+  ///////////////////////////////////////////////////////
+  
   std::cout << "Hello World!" << std::endl;
   return 0;
 }
